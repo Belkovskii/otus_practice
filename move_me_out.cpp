062d2a73caa915dacf0d234fb1f723b4c074b239
DenisBelkovskii <belkovsky.denis@yandex.ru>
2023-04-09 15:24:27 +0300

    homework_1
    Separate function realisation into .cpp and interface into .h files, set cmake CMaleLists.txt, build project

---
+++ move_me_out.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "move_me_out.h"
+
+void todo_move_me_out() {
+	std::cout << "This function is now located in separate source file, I've done my first homework!!!" << std::endl;
+}
